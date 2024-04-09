fdcb044d450eb96237295aae0fc4fc53dcb76402
SolanaExpert90 <solanaexpert90@gmail.com>
2024-04-08 20:39:10 -0700

    update bot

--- src/swapConfig.ts
+++ src/swapConfig.ts
@@ -5,7 +5,7 @@ export const swapConfig = {
   tokenAAddress: "So11111111111111111111111111111111111111112", // Token to swap for the other, SOL in this case
   tokenBAddress: "9aVMRKmN8EPjaoQorwkJJjinst4xSkHiCfRgCKWDvz4f", // token address
   poolAddress: "BhNPmUzQoNzpmijvhHmHH2HQ7dpSJh9QHBZDBuS2TE3S",
-  maxLamports: 1500000, // Micro lamports for priority fee
+  maxLamports: 500000, // Micro lamports for priority fee
   direction: "in" as "in" | "out", // Swap direction: 'in' or 'out'
   liquidityFile: "https://api.raydium.io/v2/sdk/liquidity/mainnet.json",
   maxRetries: 20,
